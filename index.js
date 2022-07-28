b464e7c2667f26d8c573ed7b6b5e8ad6986e8d64
Sheila <sheilastegman25@gmail.com>
2022-07-28 10:21:05 +0100

    add current files

--- index.js
+++ index.js
@@ -1,56 +1,62 @@
-require('dotenv').config()
-const sgMail = require('@sendgrid/mail')
-console.log(process.env.SENDGRID_API_KEY);
-const setApiKey = () => sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-//sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const fs = require("fs")
+const readline = require("readline");
+const {parse} = require('csv-parse');
+const sgMail = require('@sendgrid/mail');
+const sgHelpers = require('@sendgrid/helpers');
+const { create } = require("domain");
 
-export const sendEmail = ({emailTo, data, emailFrom}) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      setApiKey();
-      const msg = {
-        to: [emailTo],
-        from: emailFrom, // Use the email address or domain you verified above
-        subject: 'Sending with Twilio SendGrid is Fun',
-        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-      };
-      const result = await sgMail.send(msg);
-      resolve(result);
-    } catch (_e) {
-      console.log('Error in sendEmail: ', _e.response.body);
-      reject(_e.response.body);
-    }
-  });
-};
+const Personalization = sgHelpers.classes.Personalization;
+const API_KEY = '<YOUR API KEY HERE>';//Add your own SendGrid API key here
+sgMail.setApiKey(API_KEY);
+var csvData=[];//sets up array to hold the csv data read in the future
 
-app.post('/send', (req, res) => {
-  try {
-    const { email, subject, content } = req.body;
-    await sendMail(email, subject, content);
-    res.json({ message: 'Message Sent' });
-  } catch (error) {
-    res.status(400).json({ message: 'Something went wrong' });
-  }
-  console.log(req.body);
+//file reader for the csv. set to read each row as: FIRSTNAME,LASTNAME,SUBJECT,EMAIL,CAPTION,REC1,IMG1,REC2,IMG2,REC3,IMG3,REC4,IMG4,REC5,IMG5
+fs.createReadStream("UserDataRecs.csv")
+.pipe(parse({delimiter: ','}))
+.on('data', function(csvrow) {
+  console.log(csvrow);
+  //Forms the email message according to the read in data
+  let msg = {
+    from: '<VERIFIED EMAIL HERE>',//use your verified sender's email on SendGrid
+    to:csvrow[3],
+    name: csvrow[0]+" "+csvrow[1],
+    personalizations: [],
+    
+    templateId: 'd-2bcba09e8e5d411faf80df32c4e91129',
+     dynamic_template_data: {
+      subject: csvrow[2],
+      first_name: csvrow[0],
+      caption: csvrow[4],
+      
+      rec1desc:csvrow[5],
+      rec1img:csvrow[6],
+      
+      rec2desc:csvrow[7],
+      rec2img:csvrow[8],
+      
+      rec3desc:csvrow[9],
+      rec3img:csvrow[10],
+      
+      rec4desc:csvrow[11],
+      rec4img:csvrow[12],
+      
+      rec5desc:csvrow[13],
+      rec5img:csvrow[14]
+      },
+  };
+  //console.log(csvrow);
+  const personalization1 = new Personalization();
+  personalization1.setTo(csvrow[3]);
+  personalization1.setSubject(csvrow[2]);
+  msg.personalizations.push(personalization1);
+  
+//Sends out each email for the entire csv file's list
+sgMail
+.send(msg)
+.then(response => console.log('Email Sent!'))
+.catch((error)=> console.log(error.response.body));
+})
+.on('end',function() {
 });
 
 
-
-
-/*const msg = {
-  to: 'sheila.stegman@ttu.edu', // Change to your recipient
-  from: 'sheila.stegman@ttu.edu', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
-
-sgMail
-  .send(msg)
-  .then((response) => {
-    console.log(response[0].statusCode)
-    console.log(response[0].headers)
-  })
-  .catch((error) => {
-    console.error(error)
-  })*/
